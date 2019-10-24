8f8897fd376852dda19f82d22af3e381c88e6cfd
Bloom9900 <jannichhoejmose@gmail.com>
2019-10-24 18:30:43 +0200

    7

--- src/Bestilling.java
+++ src/Bestilling.java
@@ -1,24 +1,42 @@
+
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Bestilling {
- 
-    private ArrayList<Pizza> Bestilling = new ArrayList();
-    
-    
-    
-    public void tilføjPizza(Pizza pizza){
-        Bestilling.add(pizza);
-        
-        
-    }
-    public void fjernPizza(Pizza pizza){
-        Bestilling.remove(pizza);
+
+    private ArrayList<Pizza> bestilling = new ArrayList();
+
+    private String kundeNavn;
+    private int pizzaNr2;
+
+    public void tilføjPizza(Pizza pizza) {
+        bestilling.add(pizza);
+
     }
 
+    public void fjernPizza(Pizza pizza) {
+        bestilling.remove(pizza);
+    }
+
+    public void brugerDialog() {
+        Scanner myScan = new Scanner(System.in);
+        System.out.println("Tryk 1 for at tilføje en bestilling.\nEller 2 for at fjerne bestilling.");
+        int svarDialog = myScan.nextInt();
+        if (svarDialog == 1) {
+            System.out.println("Indtast kundens navn");
+            kundeNavn = myScan.nextLine();
+            System.out.println("Indtast pizza'ens nummer");
+            pizzaNr2 = myScan.nextInt();
+            if(pizzaNr==pizzaNr2) {
+                tilføjPizza(pizza);
+            }
+        }
+    }
+    /*
     @Override
     public String toString() {
         String result = "Orderen indeholder:\n";
-        for (Pizza pizza: Bestilling){
+        for (Pizza pizza: bestilling){
             if (pizza.getAntal()>1){
                 result+=pizza.getAntal()+" Pizzaer nr: "+pizza.getPizzaNr()+"\n";
             }else{
@@ -29,10 +47,5 @@ public class Bestilling {
         }
         return result;
     }
+    */
 }
-    
-
-
-        
-    
-
