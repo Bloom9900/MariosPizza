6214fa628e60b5786407351cec2982ca9d24374b
Bloom9900 <jannichhoejmose@gmail.com>
2019-10-28 10:00:01 +0100

    18

---
+++ src/Filewriter.java
@@ -0,0 +1,20 @@
+
+import java.io.FileWriter;
+
+/*
+ * To change this license header, choose License Headers in Project Prop
+erties
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jannich
+ */
+public class Filewriter extends FileWriter {
+
+    public Filewriter(String mariosStatistikcsv) {
+    }
+    
+}
